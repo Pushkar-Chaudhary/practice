26a86c1112b6ffe32d9f3c32adea209199b75938
unknown <pushkarchaudhary256@gmail.com>
2026-09-18 20:03:04 +0545

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const path = require('path')
+const app=express();
+app.set("view engine","ejs")
+app.use(express.json());
+app.listen(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")))
+app.get("/",function(req,res){
+    res.send("welcome");
+})
+app.listen(3000);
