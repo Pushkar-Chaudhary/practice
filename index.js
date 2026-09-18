174f15ff88ce5780844e4f71ce2d8d5708ce50d1
unknown <pushkarchaudhary256@gmail.com>
2026-09-18 20:14:13 +0545

    new

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express= require('express');
-const path = require('path')
-const app=express();
-app.set("view engine","ejs")
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.listen(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"public")))
-app.get("/",function(req,res){
-    res.send("welcome");
-})
-app.listen(3000);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.send('welcome');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
